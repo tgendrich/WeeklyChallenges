15ac10db7eef8c2a03b51828bbbcfc6e1faa60a5
tgendrich <gendricht@gmail.com>
2021-08-03 20:08:32 -0500

    third set finished

--- ChallengesWithTestsMark8/ChallengesSet03.cs
+++ ChallengesWithTestsMark8/ChallengesSet03.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace ChallengesWithTestsMark8
 {
@@ -7,47 +8,104 @@ namespace ChallengesWithTestsMark8
     {
         public bool ArrayContainsAFalse(bool[] vals)
         {
-            throw new NotImplementedException();
+            Array.Sort(vals);
+
+            if (vals == null || vals.Length==0)
+                return false;
+
+            else if (vals[0] == false)
+            {
+                return true;
+            }
+            else
+                return false;       
+            
+            
         }
 
         public bool IsSumOfOddsOdd(IEnumerable<int> numbers)
         {
-            throw new NotImplementedException();
+            var sumer = 0;
+            if (numbers == null)
+                return false;
+            foreach (int i in numbers)
+            {
+                if (i % 2 != 0)
+                {
+                    sumer += i;
+                }
+                else
+                    continue;
+            }
+            if (sumer % 2 != 0)
+                return true;
+            else
+                return false;
+            
         }
 
         public bool PasswordContainsUpperLowerAndNumber(string password)
         {
-            throw new NotImplementedException();
+            if (password.Any(char.IsLower) && password.Any(char.IsUpper) && password.Any(char.IsDigit) == true)
+                return true;
+            else
+                return false;
         }
 
         public char GetFirstLetterOfString(string val)
         {
-            throw new NotImplementedException();
+            return val[0];
+            
         }
 
         public char GetLastLetterOfString(string val)
         {
-            throw new NotImplementedException();
+            var lastIndex = val.Length;
+            return val[lastIndex-1];
+            
         }
 
         public decimal Divide(decimal dividend, decimal divisor)
         {
+            if (divisor == 0)
+                return 0;
+            
+            else
+                return dividend / divisor;
             throw new NotImplementedException();
         }
 
         public int LastMinusFirst(int[] nums)
         {
-            throw new NotImplementedException();
+            return nums[nums.Length - 1] - nums[0];
         }
 
         public int[] GetOddsBelow100()
         {
+            List<int> oddsBelow100 = new List<int>();
+            for (int i = 0; i < 100; i++)
+            {
+                if (i % 2 == 0)
+                    continue;
+                else
+                    oddsBelow100.Add(i);
+            }
+
+
+            return oddsBelow100.ToArray();
             throw new NotImplementedException();
         }
 
         public void ChangeAllElementsToUppercase(string[] words)
         {
-            throw new NotImplementedException();
+            for (int i = 0; i < words.Length; i++)
+            {
+                words[i] = words[i].ToUpper();
+            }
+
+            
+            
+            
         }
     }
 }
--- ChallengesWithTestsMark8/ChallengesSet06.cs
+++ ChallengesWithTestsMark8/ChallengesSet06.cs
@@ -12,13 +12,58 @@ namespace ChallengesWithTestsMark8
 
         public bool IsPrimeNumber(int num)
         {
+            
+            if (num <= 1)
+                return false;
+            for (int i = 2; i < num; i++)
+            {
+                if (num % i == 0)
+                    return false;
+            }
+            return true;
+
+            
             throw new NotImplementedException();
         }
 
         public int IndexOfLastUniqueLetter(string str)
         {
-            throw new NotImplementedException();
+            /*
+                256 represents the total number of chars that can be frequenced. the array freq allows one to add a
+                numerical value to each character in the string. Since we want the last non repeated char,
+                we count down instead of up in the second for loop to find the last instant of 1, or the last single unqiue character. 
+                Run IndexOf in order to find the index, rather than the actual character.
+            
+             */
+            
+            
+            int maxPossibleChars = 256;
+            int[] freq = new int[maxPossibleChars];
+
+           
+            for (int i = 0; i < str.Length; i++)
+                freq[str[i]]++;
+
+        
+            for (int i = str.Length - 1; i >= 0; i--)
+            {
+
+               
+                char ch = str[i];
+
+                
+                if (freq[ch] == 1)
+                {
+                    
+                    return str.IndexOf(ch);
+                }
+
+            }
+
+            
+            return -1;
         }
+    
 
         public int MaxConsecutiveCount(int[] numbers)
         {
